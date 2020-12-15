9100211f2c75cf6499bcef50b94263beef86b7d1
JonCourbon <joncourbon@gmail.com>
2020-12-15 18:51:54 +0100

    Ajout step3: récuperer data; ajout step 4: fonctions

---
+++ index_step4.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <title>LISTING ETUDIANTS</title>
+  
+  <!-- jQuery-->
+  <script src="js/jquery-3.4.1.min.js"></script>
+  
+  <link rel="stylesheet" href="css/fichestyle.css">
+  
+</head>
+<body>
+  <button type="button" data-promo="mmi1">MMI1</button> 
+  <button type="button" data-promo="mmi2">MMI2</button> 
+  <main>  
+    <div class="wrapper">
+      <div class="stud">
+        <h2>Etudiant 1</h2>
+        <p>Nom1 prenom1</p>
+        <p>Mail1</p>
+      </div>
+      <div class="stud">
+        <h2>Etudiant 2</h2>
+        <p>Nom1 prenom2</p>
+        <p>Mail2</p>
+      </div>
+      <div class="stud">
+        <h2>Etudiant 3</h2>
+        <p>Nom1 prenom3</p>
+        <p>Mail3</p>
+      </div>
+      <div class="stud">
+        <h2>Etudiant 4</h2>
+        <p>Nom1 prenom4</p>
+        <p>Mail4</p>
+      </div>
+    </div>
+  </main>
+  
+</body>
+
+<script type="text/javascript">
+function afficherEtudiants(lapromo){
+  $.getJSON( "API/recupererEtudiants.php",{promo:lapromo}, function( data ) {
+    if(data["status"]=="OK"){
+      var listeStud="";
+      for (index = 0; index < data["etudiants"].length; index++) {
+        listeStud=listeStud+
+        '<div class="stud">'+
+        '  <h2>Etudiant '+data["etudiants"][index].id+'</h2>'+
+        '  <p>'+data["etudiants"][index].nom+' '+data["etudiants"][index].prenom+'</p>'+
+        '  <p>Mail:'+data["etudiants"][index].mail+'</p>'+
+        '</div>';
+      }
+      $(".wrapper").html(listeStud);
+    }          
+  });
+}
+
+$("button").click( function(){
+  var promo=$(this).data("promo");
+  afficherEtudiants(promo);
+});
+
+</script>
+
+</html>
